0b9ea4d6c931c19866cf489d4e5f3e21f6b387e0
Escalos <stornblood6969@gmail.com>
2022-06-07 12:57:29 -0500

    Haciendo cars

--- app/Http/Livewire/RequestMaterial.php
+++ app/Http/Livewire/RequestMaterial.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Component as ModelsComponent;
+use App\Models\Implement;
 use App\Models\OrderRequest;
 use App\Models\OrderRequestDetail;
 use Livewire\Component;
@@ -12,57 +14,28 @@ class RequestMaterial extends Component
     use WithPagination;
 
     public $open;
-    public $idImplement=0;
-    public $inputs = [];
-
-    public function updateOrderRequest($order_request, $quantity){
-        $order_request = OrderRequestDetail::find($order_request);
-        $order_request->quantity = $quantity;
-        $order_request->save();
-        $this->render();
-    }
-
-    public function abrir_modal($id){
-        $this->idImplement = $id;
-        foreach(OrderRequestDetail::where('order_request_id',$id)->get() as $item){
-            array_push($this->inputs,[
-                "id" => $item->id,
-                "componente" => $item->item->item,
-                "quantity"=>$item->quantity,
-            ]);
-        }
-        $this->open = true;
-    }
+    public $idRequest = 0;
+    public $implemento;
 
     public function cerrar(){
         $this->open = false;
-        $this->inputs = [];
     }
 
-    public function increase($index){
-        $componente = $this->inputs[$index]['id'];
-        $this->inputs[$index]['quantity']+=1;
-        $this->actualizar($componente,$this->inputs[$index]['quantity']);
-    }
-
-    public function reduce($index){
-        if($this->inputs[$index]['quantity'] > 0){
-            $componente = $this->inputs[$index]['id'];
-            $this->inputs[$index]['quantity']-=1;
-            $this->actualizar($componente,$this->inputs[$index]['quantity']);
-        }
-    }
-
-    public function actualizar($id,$cantidad){
-        $order_request = OrderRequestDetail::find($id);
-        $order_request->quantity = $cantidad;
-        $order_request->save();
-    }
-    
     public function render()
-    {   
-        $implements = OrderRequest::where('user_id',auth()->user()->id)->get();
-        
-        return view('livewire.request-material',compact('implements'));
+    {
+        if($this->idRequest>0){
+            $request = OrderRequest::where('id',$this->idRequest)->first();
+            $implement = Implement::where('id',$request->implement->id)->first();
+            $this->implemento = $implement->implementModel->implement_model.' '.$implement->implement_number;
+        }else{
+            $this->implemento = "Seleccione un implemento";
+        }
+        $requests = OrderRequest::where('user_id',auth()->user()->id)->get();
+        //$components = ModelsComponent::whereRelation('implements','implement_id',$this->idImplement)->get();
+
+        $components = OrderRequestDetail::where('order_request_id',$this->idRequest)->get();
+
+
+        return view('livewire.request-material',compact('requests','components'));
     }
 }
--- resources/views/livewire/request-material.blade.php
+++ resources/views/livewire/request-material.blade.php
@@ -1,67 +1,187 @@
 <div>
     <div class="text-center">
-        <h1 class="text-2xl font-bold pb-4">Solicitud de Pedido</h1>
+        <h1 class="text-2xl font-bold pb-4">Solicitud de Pedido : {{$implemento}}</h1>
     </div>
-    <div class="px-6 py-4 grid grid-cols-1 sm:grid-cols-3" wire:ignore>
-        @foreach ($implements as $implement)
-        <div class="p-6 mb-4 ml-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $implement->implement->implementModel->implement_model }} {{ $implement->implement->implement_number }} </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-            <button type="button" wire:click="abrir_modal({{$implement->id}})" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Ver detalle
-                <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-        </div>
+    <div class="py-2" style="padding-left: 1rem; padding-right:1rem">
+        <select class="form-select" style="width: 100%" wire:model='idRequest'>
+            <option value="0">Seleccione una implemento</option>
+        @foreach ($requests as $request)
+            <option value="{{ $request->id }}">{{ $request->implement->implementModel->implement_model }} {{ $request->implement->implement_number }}</option>
         @endforeach
+        </select>
     </div>
-    <div>
-        <x-jet-dialog-modal wire:model="open">
-            <x-slot name="title">
-                Lista de Componentes
-            </x-slot>
-            <x-slot name="content">
-                <table class="min-w-max w-full table-fixed overflow-x-scroll">
+    <div class="px-6 py-4">
+        @if ($components->count())
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div style="height:180px;overflow:auto">
+                <table class="min-w-max w-full">
                     <thead>
                         <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
                             <th class="py-3 text-center">
-                                <span class="hidden sm:block">Componente</span>
+                                <span>Componentes</span>
                             </th>
                             <th class="py-3 text-center">
-                                <span class="hidden sm:block">Cantidad</span>
+                                <span>Cantidad</span>
                             </th>
                         </tr>
                     </thead>
-                    <tbody>
-                        @foreach ($inputs as $componente)
-                        <tr class="border-b border-gray-200">
-                            <td class="py-3 px-6 text-left">
-                                <div class="flex items-center">
-                                    <span class="font-medium">{{ $inputs[$loop->index]['componente']}}</span>
-                                </div>
-                            </td>
-                            <td class="py-3 px-6 text-left">
-                                <button class="px-4 py-2 bg-blue-500 rounded-md" wire:click="increase({{$loop->index}})">+</button>
-                                <x-jet-input class="text-center" type="text" style="height:30px;width: 66%" wire:model="inputs.{{ $loop->index }}.quantity"/>
-                                <button class="px-4 py-2 bg-red-500 rounded-md" wire:click="reduce({{$loop->index}})">-</button>
-                            </td>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($components as $component)
+                            <tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr><tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr>
                         @endforeach
                     </tbody>
                 </table>
-            </x-slot>
-            <x-slot name="footer">
-                <x-jet-button wire:loading.attr="disabled" wire:click="addItems()">
-                    Guardar
-                </x-jet-button>
-                <x-jet-danger-button class="ml-2" wire:loading.attr="disabled" wire:click="cerrar()">
-                    Cerrar
-                </x-jet-danger-button>
-                <div wire:loading wire:target="addItems">
-                    Registrando...
-                </div>
-                <x-jet-secondary-button wire:click="cerrar" class="ml-2">
-                    Cancelar
-                </x-jet-secondary-button>
-            </x-slot>
-        </x-jet-dialog-modal>
+            </div>
+            <div style="height:180px;overflow:auto">
+                <table class="min-w-max w-full">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <th class="py-3 text-center">
+                                <span>Componentes</span>
+                            </th>
+                            <th class="py-3 text-center">
+                                <span>Cantidad</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($components as $component)
+                            <tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr><tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div style="height:180px;overflow:auto">
+                <table class="min-w-max w-full">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <th class="py-3 text-center">
+                                <span>Componentes</span>
+                            </th>
+                            <th class="py-3 text-center">
+                                <span>Cantidad</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($components as $component)
+                            <tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr><tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div style="height:180px;overflow:auto">
+                <table class="min-w-max w-full">
+                    <thead>
+                        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                            <th class="py-3 text-center">
+                                <span>Componentes</span>
+                            </th>
+                            <th class="py-3 text-center">
+                                <span>Cantidad</span>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-gray-600 text-sm font-light">
+                        @foreach ($components as $component)
+                            <tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr><tr style="cursor:pointer" class="border-b border-gray-200">
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->item->item }}</span>
+                                    </div>
+                                </td>
+                                <td class="py-3 px-6 text-center">
+                                    <div>
+                                        <span class="font-medium">{{ $component->id }}</span>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        @else
+        <div class="px-6 py-4 text-center">
+            <h1 class="text-2xl font-bold pb-4">NNINGÚN IMPLEMENTO SELECCIONADO</h1>
+        </div>
+        @endif
     </div>
 </div>
