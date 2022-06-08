b0b2819d1c88f0f5383f3054d30ce4f8e3bb04d3
Escalos <stornblood6969@gmail.com>
2022-06-08 14:23:44 -0500

    Se termino detalles de la bd, se arregló el incremento de horas de componentes y piezas,

---
+++ database/migrations/2022_06_08_184844_create_affected_by_warehouse_movement_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affected_by_warehouse_movement', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('operator_stock_id')->nullable()->constrained();
+            $table->foreignId('operator_stock_detail_id')->nullable()->constrained();
+            $table->foreignId('operator_assigned_stock_id')->nullable()->constrained();
+            $table->foreignId('stock_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affected_by_warehouse_movement');
+    }
+};
