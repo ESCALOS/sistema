bf3f8d6f55106e45043a38750a5df33ddc5452c6
Escalos <stornblood6969@gmail.com>
2022-06-16 14:26:16 -0500

    haciendo cards para validacion de usuarios

--- app/Http/Livewire/ValidateRequestMaterial.php
+++ app/Http/Livewire/ValidateRequestMaterial.php
@@ -38,14 +38,12 @@ class ValidateRequestMaterial extends Component
 
         $sedes = Sede::where('zone_id',$this->tzone)->get();
 
-
         $locations = Location::where('sede_id',$this->tsede)->get();
 
         $order_requests = OrderRequest::join('implements',function ($join){
             $join->on('order_requests.implement_id','=','implements.id')
                     ->where('implements.location_id',$this->tlocation);
         })->where('state','CERRADO')->get();
-
 
         if($order_requests != null){
             foreach($order_requests as $order_request){
