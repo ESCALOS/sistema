954e0721b671959879900ad1a99e2b803261f3cc
Escalos <stornblood6969@gmail.com>
2022-06-20 23:00:40 -0500

    as

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\UserController;
 
-Route::get('',[HomeController::class,'index']);
+Route::get('',[HomeController::class,'index'])->name('admin');
 
 Route::middleware([
     'auth:sanctum',
